2c089c3135acd5ffcb4e85e20e67db46b14b9515
Pim Zonneveld <pimzonneveld@gmail.com>
2018-06-12 15:16:09 +0200

    CRUD doses

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+  get 'doses/index'
+  get 'doses/show'
+  get 'doses/new'
+  get 'doses/controller'
+  get 'doses/edit'
+  get 'doses/update'
+  get 'doses/destroy'
   root 'cocktails#index'
 
-  resources :cocktails
+  resources :cocktails do
+    resources :doses
+  end
   resources :ingredients
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
