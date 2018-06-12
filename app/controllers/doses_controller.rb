7f378b5367be1aa52afda35162b3070ae57c24a8
Pim Zonneveld <pimzonneveld@gmail.com>
2018-06-12 22:10:06 +0200

    Complete CRUD for dose, ingredient and cocktail

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -6,9 +6,17 @@ class DosesController < ApplicationController
   end
 
   def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
   end
 
-  def controller
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+
+    new_dose = Dose.create(dose_params)
+    new_dose.cocktail = @cocktail
+    new_dose.save!
+    redirect_to (@cocktail)
   end
 
   def edit
@@ -18,5 +26,15 @@ class DosesController < ApplicationController
   end
 
   def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail_id)
   end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id, :dose_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,6 @@
 Rails.application.routes.draw do
-  get 'doses/index'
-  get 'doses/show'
-  get 'doses/new'
-  get 'doses/controller'
-  get 'doses/edit'
-  get 'doses/update'
-  get 'doses/destroy'
-  root 'cocktails#index'
-
   resources :cocktails do
-    resources :doses
+    resources :doses, only: [:new, :create, :destroy, :show]
   end
   resources :ingredients
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
